39f5bd555d273e1bc3ca0a22c26d382abb824dbe
fengzanfeng <fengzanfeng@wandoujia.com>
2012-07-21 16:35:07 +0800

    update js

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,5 +1,15 @@
-(function(){
- 	var picNodes = $("div.feed div.feed-photo");
- 	console.log(picNodes.length);
- 	alert(picNodes.length);
-})();
+window.setInterval(update_home_dom, 5000);
+
+//(function() {
+//    $("#header-holder").css("display:none"); 
+//}
+//)();
+
+function update_home_dom() {
+    $("#header-holder").remove();
+    var anchors = document.getElementsByTagName("a");
+    for (var i = 0; i < anchors.length; i++) {
+        var anchor = anchors[i];
+        anchor.target = "fengzanfeng";
+    }
+}
