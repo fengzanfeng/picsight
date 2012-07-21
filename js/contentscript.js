3397ab6acdb4bdba87c985af274afb815f04a63f
bucklee <buckfengyan@gmail.com>
2012-07-21 20:05:10 +0800

    update scripts

--- js/contentscript.js
+++ js/contentscript.js
@@ -12,8 +12,46 @@
 	//generate relative tages
 
 	//generate pic download link
+	function getEachBigPicUrl(node){
+		var jsText = node.text;
+		var largePattern = /'large':'(.*)'/gm;
+		// var rs = largePattern.exec(jsText);
+		var largePicUrlArray = jsText.match(largePattern);
+		return largePicUrlArray.join(",").replace(/'large':/g,"").replace(/'/g,"").split(",");
+	}
+
+    function setDownloadLinkForEach(){
+    	var parent = $("div.feed.feed-photo");
+    	var firstPicUrl = getEachBigPicUrl(parent.find("script")[0])[0];
+    	var picNodes = parent.find("div.feed-act");
+    	for(var i=0;i<picNodes.length;i++){
+    		var downloadLink = $("<a href='"+firstPicUrl+"#name=test.jpg' rel='download'>下载图片</a>");
+    		downloadLink.insertBefore(picNodes[i].firstChild);
+    	}
+    }
+    setDownloadLinkForEach();
 
 	//generate all-pic download link
+	function getAllBigPicUrl(){
+		var picNodes = $("div.feed.feed-photo script");
+		var result = []; 
+		for(var i=0;i<picNodes.length;i++){
+			var jsText = picNodes[i].text;
+			var largePattern = /'large':'(.*)'/gm;
+			// var rs = largePattern.exec(jsText);
+			var largePicUrlArray = jsText.match(largePattern);
+			result[i] = largePicUrlArray;
+		}
+		var flattenUrl = result.join(",").replace(/'large':/g,"").replace(/'/g,"");
+		var flattenUrlArray = flattenUrl.split(",");
 
+		//
+		i=0;
+		for(;i<flattenUrlArray.length;i++){
+
+		}
+	}
+
+	//getAllBigPicUrl();
 
 })();
