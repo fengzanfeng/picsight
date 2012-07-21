0f4c2e4bcb7beade0cdd185daa84a9da63e44d01
fengzanfeng <fengzanfeng@wandoujia.com>
2012-07-21 17:38:09 +0800

    update scripts

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,13 +1,21 @@
 (function(){
 	//remove head
+    $("#header-holder").remove();
+    $("div.hot-tags-holder").remove();
 
 	//remove login form
+    $("#aside-reg-login").remove();
+    $("#js-top-contributor").remove();
+    $("#js-top-editor").remove();
 
 	//remove hot-tags-holder
 
 	//reposition search box to top
+    var search_box = $("#tag-search-outer");
+    $("#tag-search-outer").prev().before($("#tag-search-outer"));
 
 	//remove head pics
+    $("div.feed-avatar").remove();
 
 	//generate relative tages
 
